75dbb56ca4d530f00ac6747d4eefd89a35d8e194
肝狱 <43373472+chenjunshuo@users.noreply.github.com>
2018-11-12 23:46:57 +0800

    Add files via upload

---
+++ test.11.12.电话簿/test.11.12.电话簿/PhoneBook.h
@@ -0,0 +1,33 @@
+#ifndef  __PhoneBook_h__
+#define  __PhoneBook_h__
+
+#define  SIZE 1000
+
+typedef struct Contact
+{
+	char name[1024];
+	char phone[1024];
+}Contact;
+
+typedef struct PhoneBook
+{
+	Contact contact[SIZE];
+	int size;
+}PhoneBook;
+
+PhoneBook phonebook;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void clean();
+void Init();
+void AddContact();
+void DelContact();
+void ModifyContact();
+void FindContact();
+void PrintContact();
+void CleanContact();
+
+#endif  //__PhoneBook_h__
